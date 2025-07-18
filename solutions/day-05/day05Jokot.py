81052f5b66fc018dc16ad89714d4d76945eedaae
jokot <jokotriyanto12345@gmail.com>
2025-07-18 09:04:35 +0700

    refactor: optimize top K frequent elements solution in Python
    - Replaced bucket sort implementation with a more efficient approach using the Counter class from collections.
    - Simplified the method to retrieve the top K frequent elements by directly using most_common.
    - Removed unnecessary helper methods for counting and bucket sorting, streamlining the code.

--- solutions/day-05/day05Jokot.py
+++ solutions/day-05/day05Jokot.py
@@ -1,35 +1,14 @@
-from collections import defaultdict
+from collections import Counter
+from typing import List
 
 class Solution:
-    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        if len(nums) == k:
-            return nums
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        dic = self.counter(nums)
-        buckets = self.bucket_sort(len(nums), dic)
-        top = self.get_top(buckets, k)
+        n = Counter(nums)
+
+        top = []
+        
+        for n in n.most_common(k):
+            top.append(n[0])
 
         return top
-    
-    def get_top(self, buckets, k):
-        top = []
-        for b in buckets[::-1]:
-            for e in b:
-                top.append(e)
-                if len(top) == k:
-                    return top
-    
-    def counter(self, nums):
-        count = defaultdict(int)
-        for n in nums:
-            count[n] += 1
-        
-        return count
-
-    def bucket_sort(self, size, dic):
-        buckets = [[] for _ in range(size+1)]
-
-        for key, val in dic.items():
-            buckets[val].append(key)
-
-        return buckets
